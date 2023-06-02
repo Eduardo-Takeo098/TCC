7e73df4931a1067b4f5626d1482463d12d6cf480
Eduardo-Takeo098 <eduardotakeo098@gmail.com>
2023-06-01 22:28:58 -0300

    Rota Criada

--- tcc/src/screens/MapScreen/index.js
+++ tcc/src/screens/MapScreen/index.js
@@ -1,205 +1,183 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import MapView, { Marker, Polyline } from 'react-native-maps';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import {StyleSheet, View, Dimensions, Text, TouchableOpacity} from "react-native";
+import {GooglePlacesAutocomplete,} from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "./environments";
+import Constants from "expo-constants";
+import { useRef, useState } from "react";
+import MapViewDirections from "react-native-maps-directions";
+import React from "react";
 
-const MapScreen = () => {
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 40.76711,
+  longitude: -73.979704,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
+
+function InputAutocomplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <GooglePlacesAutocomplete
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder || ""}
+        fetchDetails
+        onPress={(data, details = null) => {
+          onPlaceSelected(details);
+        }}
+        query={{
+          key: GOOGLE_API_KEY,
+          language: "pt-BR",
+        }}
+      />
+    </>
+  );
+}
+
+export default function App() {
   const [origin, setOrigin] = useState(null);
   const [destination, setDestination] = useState(null);
-  const [routeCoordinates, setRouteCoordinates] = useState([]);
-  const [directionSteps, setDirectionSteps] = useState([]);
+  const [showDirections, setShowDirections] = useState(false);
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const mapRef = useRef(null);
 
-  useEffect(() => {
+  const moveTo = async (position) => {
+    const camera = await mapRef.current?.getCamera();
+    if (camera) {
+      camera.center = position;
+      mapRef.current?.animateCamera(camera, { duration: 1000 });
+    }
+  };
+
+  const edgePaddingValue = 70;
+
+  const edgePadding = {
+    top: edgePaddingValue,
+    right: edgePaddingValue,
+    bottom: edgePaddingValue,
+    left: edgePaddingValue,
+  };
+
+  const traceRouteOnReady = (args) => {
+    if (args) {
+      setDistance(args.distance);
+      setDuration(args.duration);
+    }
+  };
+
+  const traceRoute = () => {
     if (origin && destination) {
-      getDirections();
+      setShowDirections(true);
+      mapRef.current?.fitToCoordinates([origin, destination], { edgePadding });
     }
-  }, [origin, destination]);
-
-  const handleOriginSelect = (data, details) => {
-    const { description, geometry } = details;
-    const { location } = geometry;
-
-    setOrigin({
-      description,
-      latitude: location.lat,
-      longitude: location.lng,
-    });
   };
 
-  const handleDestinationSelect = (data, details) => {
-    const { description, geometry } = details;
-    const { location } = geometry;
-
-    setDestination({
-      description,
-      latitude: location.lat,
-      longitude: location.lng,
-    });
+  const onPlaceSelected = (
+    details,
+    flag
+  ) => {
+    const set = flag === "origin" ? setOrigin : setDestination;
+    const position = {
+      latitude: details?.geometry.location.lat || 0,
+      longitude: details?.geometry.location.lng || 0,
+    };
+    set(position);
+    moveTo(position);
   };
-
-  const getDirections = () => {
-    const originLatLng = `${origin.latitude},${origin.longitude}`;
-    const destinationLatLng = `${destination.latitude},${destination.longitude}`;
-
-
-    const url = `http://localhost:3000/directions?origin=${originLatLng}&destination=${destinationLatLng}`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        if (data.status === 'OK') {
-          const { routes } = data;
-          const { overview_polyline, legs } = routes[0];
-          const { steps } = legs[0];
-
-          const coordinates = decodePolyline(overview_polyline.points);
-
-          setRouteCoordinates(coordinates);
-          setDirectionSteps(steps);
-        } else {
-          console.error('Error getting directions:', data);
-        }
-      })
-      .catch(error => {
-        console.error('Error getting directions:', error);
-      });
-  };
-
-  const decodePolyline = polyline => {
-    const points = [];
-    let index = 0;
-    const len = polyline.length;
-    let lat = 0;
-    let lng = 0;
-
-    while (index < len) {
-      let b;
-      let shift = 0;
-      let result = 0;
-
-      do {
-        b = polyline.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-
-      const dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-
-      shift = 0;
-      result = 0;
-
-      do {
-        b = polyline.charCodeAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-
-      const dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-
-      points.push({
-        latitude: lat / 1e5,
-        longitude: lng / 1e5,
-      });
-    }
-
-    return points;
-  };
-
-  const getMapRegion = () => {
-    if (routeCoordinates.length > 0) {
-      const latitudes = routeCoordinates.map(coordinate => coordinate.latitude);
-      const longitudes = routeCoordinates.map(coordinate => coordinate.longitude);
-      const minLat = Math.min(...latitudes);
-      const maxLat = Math.max(...latitudes);
-      const minLng = Math.min(...longitudes);
-      const maxLng = Math.max(...longitudes);
-
-      const margin = 0.1;
-
-      return {
-        latitude: (minLat + maxLat) / 2,
-        longitude: (minLng + maxLng) / 2,
-        latitudeDelta: (maxLat - minLat) + margin,
-        longitudeDelta: (maxLng - minLng) + margin,
-      };
-    }
-
-    return null;
-  };
-
+  
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
-        initialRegion={origin && { ...origin, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
-        region={getMapRegion()}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_POSITION}
       >
-        {origin && <Marker coordinate={origin} title="Origem" />}
-        {destination && <Marker coordinate={destination} title="Destino" />}
-        {routeCoordinates.length > 0 && (
-          <Polyline coordinates={routeCoordinates} strokeWidth={4} strokeColor="#1a66ff" />
+        {origin && <Marker coordinate={origin} />}
+        {destination && <Marker coordinate={destination} />}
+        {showDirections && origin && destination && (
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_API_KEY}
+            strokeColor="#6644ff"
+            strokeWidth={4}
+            onReady={traceRouteOnReady}
+          />
         )}
       </MapView>
       <View style={styles.searchContainer}>
-        {/* Componente para selecionar a origem */}
-        <GooglePlacesAutocomplete
-          placeholder="Sua localização"
-          onPress={handleOriginSelect}
-          fetchDetails
-          query={{
-            key: 'AIzaSyDnp6LRTU4hYJM0XjY57ywva2hfmiLweH4',
-            language: 'pt-BR',
+        <InputAutocomplete
+          label="Origin"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "origin");
           }}
         />
-        {/* Componente para selecionar o destino */}
-        <GooglePlacesAutocomplete
-          placeholder="Seu destino"
-          onPress={handleDestinationSelect}
-          fetchDetails
-          query={{
-            key: 'AIzaSyDnp6LRTU4hYJM0XjY57ywva2hfmiLweH4',
-            language: 'pt-BR',
+        <InputAutocomplete
+          label="Destination"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "destination");
           }}
         />
-      </View>
-      <View style={styles.directionsContainer}>
-        {directionSteps.map((step, index) => (
-          <View key={index} style={styles.directionStep}>
-            <Text>{step.instructions}</Text>
+        <TouchableOpacity style={styles.button} onPress={traceRoute}>
+          <Text style={styles.buttonText}>Trace route</Text>
+        </TouchableOpacity>
+        {distance && duration ? (
+          <View>
+            <Text>Distance: {distance.toFixed(2)}</Text>
+            <Text>Duration: {Math.ceil(duration)} min</Text>
           </View>
-        ))}
+        ) : null}
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
   searchContainer: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
-    right: 20,
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
   },
-  directionsContainer: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 5,
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
   },
-  directionStep: {
-    marginBottom: 10,
+  button: {
+    backgroundColor: "#bbb",
+    paddingVertical: 12,
+    marginTop: 16,
+    borderRadius: 4,
+  },
+  buttonText: {
+    textAlign: "center",
   },
 });
-
-export default MapScreen;
